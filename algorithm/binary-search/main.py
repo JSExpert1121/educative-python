66f9357348930cc058bcd545deb539d177752be5
JSExpert1121 <jingang0314@gmail.com>
2022-11-12 22:49:58 +0800

    feat: bisect

--- algorithm/binary-search/main.py
+++ algorithm/binary-search/main.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left, bisect_right, insort_left, insort_right
 from binary_search import (
     linear_search,
     binary_search_iterative,
@@ -30,10 +31,20 @@ def run_bitonic_peak():
     print(bs_find_peak_in_bitonic([1, 2, 3, 4, 5]))
     print(bs_find_peak_in_bitonic([5, 4, 3, 2, 1]))
 
+def run_bisect():
+    print(bisect_left([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 2))
+    print(bisect_right([-14, -10, 2, 108, 108, 243, 285, 285, 285, 401], 285))
+
+    A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
+    insort_left(A, 108)
+    print(A)
+    insort_right(A, 108)
+    print(A)
 
 
 if __name__ == '__main__':
     # run_search()
     # run_closest()
     # run_fixed_point()
-    run_bitonic_peak()
+    # run_bitonic_peak()
+    run_bisect()
